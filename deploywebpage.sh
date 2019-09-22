05c579b2771962c69fb3bcb7ebec1fb2c717a1dd
Johnsanjay <55642317+Johnsanjay@users.noreply.github.com>
2019-09-23 00:48:31 +0530

    Add files via upload

---
+++ deploywebpage.sh
@@ -0,0 +1,43 @@
+
+#!/bin/bash
+
+PACK="httpd wget unzip"
+WEBURL="https://www.tooplate.com/zip-templates/2113_earth.zip"
+ARTIFACT="2113_earth"
+ROOTDIR="/var/www/html/"
+
+echo "Setting up a webserver"
+
+echo "Installing packages"
+echo
+yum install -y $PACK
+
+echo
+echo "starting and enabling the httpd service"
+systemctl start httpd
+systemctl enable httpd
+echo
+echo
+
+echo "Downloading the webpage"
+wget = "$WEBURL"
+echo
+echo
+
+echo "Unziping the artifact"
+unzip -o $ARTIFACT.zip
+echo
+echo
+
+echo "copying the files to webserver location"
+cp -r $ARTIFACT/* /$ROOTDIR
+echo
+echo
+
+echo "Restartnig the webservice"
+systemctl restart httpd
+echo
+echo
+
+echo "please use the ip in webbrowser to validate the webpage"
+
